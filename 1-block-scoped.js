0a7b89047327fc96fa41a477fa1ce5c5f22ba17c
astro175 <aspark400@gmail.com>
2023-07-25 02:22:10 +0100

    added task 1

--- 1-block-scoped.js
+++ 1-block-scoped.js
@@ -1,11 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-
   if (trueOrFalse) {
-    var innertask = true;
-    var innertask2 = false;
+    //
   }
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
