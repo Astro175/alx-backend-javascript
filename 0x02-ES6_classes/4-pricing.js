9fbfb2d6bb7b0f745eb1c8f356fd55600f3b40da
astro175 <aspark400@gmail.com>
2023-07-28 21:15:52 +0100

    fixed eslint syntax error

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,38 +1,44 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-        this._currency = currency;
-        this._amount = amount;
-    }
-    // getter for amount
-    get amount() {
-        return this._amount
-    }
-    // setter for amount
-    set amount(num) {
-        if (typeof num === 'number') {
+  constructor(amount, currency) {
+    this._currency = currency;
+    this._amount = amount;
+  }
+
+  // getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // setter for amount
+  set amount(num) {
+    if (typeof num === 'number') {
       this._length = num;
     } else {
       throw new Error('Length must be a number');
     }
+  }
+
+  // getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // setter for currency
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new Error('Invalid currency. Please provide an instance of the Currency class.');
     }
-    // getter for currency 
-    get currency() {
-        return this._currency
-    }
-    // setter for currency
-    set currency(currency) {
-        if (currency instanceof Currency) {
-            this._currency = currency;
-          } else {
-            throw new Error('Invalid currency. Please provide an instance of the Currency class.');
-          }
-        }
-        displayFullPrice(){
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    }
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
-    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
