d9f738eaedc0dc3d8c6c202a7c9ad2646794c09f
astro175 <aspark400@gmail.com>
2023-07-30 12:50:53 +0100

    done task 3 and learnt about promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,6 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then(
     (value) => {
       console.log(value[0].body, value[1].firstName, value[1].lastName);
-    }, () => new Error('Signup system offline'),
-  );
+    },
+  ).catch(error, console.log('Signup system offline'));
 }
