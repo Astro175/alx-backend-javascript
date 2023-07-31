9aba8b5508e92ee4fafd0ec586973f98e3553d74
astro175 <aspark400@gmail.com>
2023-08-01 00:48:41 +0100

    fixed return dataview bug for task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
   const view1 = new DataView(buffer);
 
   if (position >= buffer.length) {
-    console.log('Error: position is out of bounds');
+    return new Error('Error: position is out of bounds');
   } else {
     view1.setInt8(position, value);
   }
