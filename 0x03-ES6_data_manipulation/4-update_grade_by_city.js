82af6a70ce09e93fc497778aedc965470289f52a
astro175 <aspark400@gmail.com>
2023-07-31 21:29:02 +0100

    added task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+export default function updateStudentGradeByCity(arr, location, newarr) {
+    const newGrades = arr.filter((student) => student.location === location).map(
+        (student) => {
+            const matchedGrade = newarr.filter((gradeobj) => gradeobj.studentId === student.id);
+            if (matchedGrade.length > 0) {
+                return { ...student, grade: matchedGrade[0].grade };
+            }
+            else {
+                return { ...student, grade: 'N/A' };
+            }
+        }
+    )
+    return newGrades;
+}
