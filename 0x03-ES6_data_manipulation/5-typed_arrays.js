07253e4f28ed24465acb03e2f1139b0b9bac983c
astro175 <aspark400@gmail.com>
2023-08-01 16:22:48 +0100

    added out of bound edge case for task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,11 +2,10 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view1 = new DataView(buffer);
 
-  if (position >= buffer.length) {
-    return new Error('Error: position is out of bounds');
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
   } else {
     view1.setInt8(position, value);
   }
-
   return view1;
 }
