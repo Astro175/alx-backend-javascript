1fd405329dd5f9dcbb73935aac58061e37b786bc
astro175 <aspark400@gmail.com>
2023-09-20 00:31:30 +0100

    fixing bugs in task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,6 +6,7 @@ const host = 'localhost';
 const requestlistener = function requestlistener(req, res) {
   res.writeHead(200);
   res.end('Hello Holberton School!');
+   res.setHeader('Content-Type', 'text/plain');
 };
 
 const server = http.createServer(requestlistener);
