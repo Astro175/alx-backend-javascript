cd031b9de5173908aba1ecd35d2a0dee28764554
astro175 <aspark400@gmail.com>
2023-09-20 05:39:04 +0100

    done task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,42 @@
 // Nodejs function to read a file async
 // Fs module with filesync module to read
 
-fs = require('fs')
-function countStudents(filepath) {
-    try {
-        data = fs.readFileSync(filepath, {encoding: 'utf-8', flag: 'r'})
-        // Splits the data into rows to count the rows
-        rows = data.split('\n');
-        console.log('Number of students:', rows.length - 1);
-        console.log(rows[3]);
-        csList = {};
-        sweList = {};
-        let cs = 0;
-        let swe = 0;
-        for (const line of rows) {
-            const fields = line.split(',')
-            if (fields[3] === 'CS')  {
-                cs += 1;
-                csList.push(fields[0])
-            }
-            else if (fields[3] === 'SWE') {
-                swe += 1;
-                sweList.push(fields[0]);
-            }
-        }
-        for (i = 0; i <= row.length - 1; i++) {
-            
-        }
+const fs = require('fs');
 
+function countStudents(filepath) {
+  try {
+    const data = fs.readFileSync(filepath, { encoding: 'utf-8', flag: 'r' });
+    // Splits the data into rows to count the rows
+    const rows = data.split('\n');
+    const courses = {};
+    const names = {};
+    for (let i = 0; i < rows.length; i += 1) {
+      // Seperates the rows into columns of fields
+      const fields = rows[i].split(',');
+      if (Object.prototype.hasOwnProperty.call(names, fields[3])) {
+        // Checks if the names object has the SWE or CS attribute and adds the names
+        names[fields[3]].push(fields[0]);
+      } else {
+        names[fields[3]] = [fields[0]];
+      }
+      // if not, adds the key and equal it the value of the name
+      if (Object.prototype.hasOwnProperty.call(courses, fields[3])) {
+        // Checks if the course object has the SWE Or CS attribute
+        courses[fields[3]] += 1;
+        // Else it adds it and gives it a value of 1
+      } else {
+        courses[fields[3]] = 1;
+      }
     }
-    catch(err) {
-        console.log('Cannot load the database');
+    console.log(`Number of students: ${rows.length - 1}`);
+    for (const [key, value] of Object.entries(courses)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${names[key].join(', ')}`);
+      }
     }
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
