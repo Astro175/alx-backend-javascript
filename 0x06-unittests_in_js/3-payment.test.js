7c506b00becfbdd7ad881999b1bbafadf9a21a20
astro175 <aspark400@gmail.com>
2023-09-22 09:51:36 +0100

    done task 4 and fixed bugs

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,15 +5,14 @@ const Utils = require("./utils");
 
 describe('Testing using Sinon to inspect the add function', () => {
   it('Checks if sendPayment uses calculateNumber', () => {
-    const sandbox = sinon.createSandbox();
 
-    const spy = sandbox.spy(Utils, 'calculateNumber');
+    const spy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(100, 20);
 
     expect(spy.calledOnce).to.be.true;
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
 
-    sandbox.restore();
+    spy.restore();
   });
 });
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -2,21 +2,23 @@ const sinon = require('sinon');
 const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require("./utils");
+const Sinon = require('sinon');
 
 describe('Testing using Sinon that stubs the add function', () => {
   it('Uses stub to make calculateNumber return 10', () => {
-    const sandbox = sinon.createSandbox();
 
-    const stub = sandbox.stub(Utils, 'calculateNumber');
-    const spy = sandbox.stub(console, 'log');
+    const stub = Sinon.stub(Utils, 'calculateNumber');
+    const spy = Sinon.stub(console, 'log');
 
-    stub.withArgs(100, 20).returns(10);
+    stub.returns(10);
 
     sendPaymentRequestToApi(100, 20);
 
     expect(stub.calledWith('SUM', 100, 20)).to.be.true;
     expect(stub.calledOnce).to.be.true;
+    expect(spy.calledWith('The total is: 10')).to.be.true;
 
-    sandbox.restore();
+    spy.restore();
+    stub.restore();
   });
 });
