3e27fdf2e868ae3bf1caf7e9f483a8bdb76b2e75
astro175 <aspark400@gmail.com>
2023-10-02 21:55:37 +0100

    fixed eslint error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,15 +1,13 @@
-function guardrail(math_function) {
-    let queue = [];
-    try {
-        const value = math_function();
-        queue.push(value);
-    }
-    catch(e) {
-        queue.push(`Error: ${e.message}`);
-    }
-    finally {
-        queue.push("Guardrail was processed");
-    }
-    return queue;
+function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch (e) {
+    queue.push(`Error: ${e.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
 module.exports = guardrail;
