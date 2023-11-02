b70c7e5d1a0589b79c77d5a7df504fec296c6d80
astro175 <aspark400@gmail.com>
2023-11-02 20:37:02 +0100

    switched to normal function, because arrow functions does not suit with mocha

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,14 +2,14 @@ const chai = require('chai');
 const sinon = require('sinon');
 const request = require('request');
 
-describe('Tests a basic express app for correct status code', () => {
+describe('Index Page', function () {
     const url = 'http://localhost:7865/';
     it('Tests the root route', () => {
         request(url, (err, response, body) => {
             chai.expect(response.statusCode).to.equal(200);
         })
     })
-    it('returns the correct message', () => {
+    it('returns the correct message', function() {
         request(url, (err, response, body) => {
             chai.expect(body).to.equal('Welcome to the payment system');
         })
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,14 +2,14 @@ const chai = require('chai');
 const sinon = require('sinon');
 const request = require('request');
 
-describe('Index Page', () => {
+describe('Index Page', function () {
   it('Tests the root route', () => {
     const url = 'http://localhost:7865/';
     request(url, (err, response, body) => {
       chai.expect(response.statusCode).to.equal(200);
     });
   });
-  it('returns the correct message', () => {
+  it('returns the correct message', function() {
     const url = 'http://localhost:7865/';
     request(url, (err, response, body) => {
       chai.expect(response.body).to.equal('Welcome to the payment system');
